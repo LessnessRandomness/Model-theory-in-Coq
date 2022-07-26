6f55b7f7f4799efd526022bf6188b5bde9f8b640
Lessness Randomness <lessnessr@gmail.com>
2022-07-26 13:21:27 +0300

    Basic definitions
    Finished writing down basic definitions.

--- start.v
+++ start.v
@@ -3,7 +3,11 @@ Require Import Utf8.
 Import EqNotations.
 Set Implicit Arguments.
 
+(* useful stuff *)
+
 Definition vmap A B n (f: A → B) (v: A^n): B^n := finfun (f \o v).
+
+(* Language, Term, Formula *)
 
 Structure Language F R := {
   function_arity: F → nat;
@@ -12,12 +16,6 @@ Structure Language F R := {
 
 Definition Function A n := A^n → A.
 Definition Relation A n := A^n → Prop.
-
-Structure Structure {F R} (L: Language F R) := {
-  domain: Type;
-  function: ∀ (f: F), Function domain (function_arity L f);
-  relation: ∀ (r: R), Relation domain (relation_arity L r)
-}.
 
 Inductive Term {F R} (V: eqType) (L: Language F R) :=
   | variable: V → Term V L
@@ -58,9 +56,20 @@ Fixpoint formula_has_free_variable F R V (L: Language F R) (A: Formula V L) (i: 
   | universal_quantifier v f => v != i ∧ formula_has_free_variable f i
   end.
 
+Definition has_n_free_variables F R V (L: Language F R) (A: Formula V L) (n: nat) :=
+  ∃ (v: V ^ n), uniq (codom v) ∧ (∀ (x: V), formula_has_free_variable A x ↔ x \in codom v).
+
 Structure Sentence F R V (L: Language F R) := {
   sentenceFormula:> Formula V L;
   sentenceProperty: ∀ i, ¬ formula_has_free_variable sentenceFormula i
+}.
+
+(* Structure, Interpretation *)
+
+Structure Structure {F R} (L: Language F R) := {
+  domain: Type;
+  function: ∀ (f: F), Function domain (function_arity L f);
+  relation: ∀ (r: R), Relation domain (relation_arity L r)
 }.
 
 Fixpoint interpreted_term F R (V: eqType) (L: Language F R) (M: Structure L) (assignment: V → domain M) (T: Term V L): domain M :=
@@ -80,10 +89,13 @@ Fixpoint satisfies F R (V: eqType) (L: Language F R) (M: Structure L) (A: Formul
   | universal_quantifier v f => ∀ i, satisfies M f (λ x, if v == x then i else assignment x) 
   end.
 
-Definition sentence_satisfies F R V (L: Language F R) (M: Structure L) (A: Sentence V L) :=
+Definition model_of_sentence F R V (L: Language F R) (M: Structure L) (A: Sentence V L) :=
   ∀ a, satisfies M A a.
 
-Definition injective A B (f: A → B) := ∀ a b, f a = f b → a = b.
+Definition model F R V (L: Language F R) (M: Structure L) (A: Sentence V L → Prop) :=
+  ∀ a, A a → model_of_sentence M a.
+
+(* Embedding, Isomorphism, Automorphism *)
 
 Structure Embedding Fm Rm Fn Rn (Lm: Language Fm Rm) (Ln: Language Fn Rn) (M: Structure Lm) (N: Structure Ln) := {
   domain_map: domain M → domain N;
@@ -98,22 +110,58 @@ Structure Embedding Fm Rm Fn Rn (Lm: Language Fm Rm) (Ln: Language Fn Rn) (M: St
     relation N (relation_map r) (vmap domain_map (rew [λ W, (domain M ^ W)%type] (relation_arity_preserved r) in v));
 }.
 
+Structure Isomorphism Fm Rm Fn Rn (Lm: Language Fm Rm) (Ln: Language Fn Rn) (M: Structure Lm) (N: Structure Ln) := {
+  isomorphism_emb: Embedding M N;
+  isomorphism_emb_inv: Embedding N M;
+  isomorphism_property_1: ∀ x, domain_map isomorphism_emb (domain_map isomorphism_emb_inv x) = x;
+  isomorphism_property_2: ∀ x, domain_map isomorphism_emb_inv (domain_map isomorphism_emb x) = x
+}.
 
-Definition has_n_free_variables F R V (L: Language F R) (A: Formula V L) (n: nat) :=
-  ∃ (v: V ^ n), uniq (codom v) ∧ (∀ (x: V), formula_has_free_variable A x ↔ x \in codom v).
+Definition Auomorphism F R (L: Language F R) (M: Structure L) := Isomorphism M M.
+
+(* Definable set *)
 
 Definition replace_variables {V: eqType} M n (v: V ^ n) (m: M ^ n) (assignment: V → M): V → M.
 Proof.
   intro x. remember (x \in codom v) as W. destruct W.
   + refine (m _). exists (index x (codom v)). 
-    assert (size (codom v) = n). { rewrite size_codom. simpl. apply card_ord. }
-    assert (index x (codom v) < size (codom v)). { rewrite index_mem. auto. }
-    rewrite H in H0. auto.
+    assert (size (codom v) = n) by abstract(rewrite size_codom; simpl; apply card_ord).
+    assert (index x (codom v) < size (codom v)) by abstract(rewrite index_mem; auto).
+    abstract (rewrite H in H0; auto).
   + exact (assignment x).
 Defined.
 
 Definition definable F R (L: Language F R) (M: Structure L) n (X: domain M ^ n → Prop) :=
   ∃ (V: eqType) (A: Formula V L), has_n_free_variables A n ∧
      ∀ (a: V → domain M) (v: V ^ n), (∀ (m: domain M ^ n), X m ↔ satisfies M A (replace_variables v m a)).
+
+(* Theory, Entailment, Satisfiable, Deductively closed *)
+
+Definition Theory F R V (L: Language F R) (C: Structure L → Prop): Sentence V L → Prop :=
+  λ (A: Sentence V L), ∀ (M: Structure L), C M → model_of_sentence M A.
+
+Definition satisfiable_theory F R (L: Language F R) (C: Structure L → Prop) :=
+  ∃ V (M: Structure L), model M (Theory C) (V:=V).
+
+Definition entailment F R V (L: Language F R) (S: Sentence V L → Prop) (A: Sentence V L) :=
+  ∀ (M: Structure L), model M S → model_of_sentence M A.
+
+Definition deductively_closed F R V (L: Language F R) (S: Sentence V L → Prop) :=
+  ∀ (A: Sentence V L), entailment S A.
+
+Definition deductive_closure F R V (L: Language F R) (S: Sentence V L → Prop): Sentence V L → Prop :=
+  λ (A: Sentence V L), entailment S A.
+
+(* Completeness *)
+
+Definition complete F R V (L: Language F R) (S: Sentence V L → Prop) :=
+  ∀ (A: Sentence V L), entailment S A ∨ ¬ entailment S A.
+
+(* Elementary equivalence *)
+
+Definition elementary_equivalence F R (L: Language F R) (M N: Structure L) :=
+  ∀ V (A: Sentence V L), model_of_sentence M A ↔ model_of_sentence N A.
+
+
 
 
